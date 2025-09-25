07a2f291af01356d96809e811f2bf8a934568b79
joelmatthew14 <jwebsite00@gmail.com>
2025-09-25 11:03:33 +0200

    Update 01.header-sidebar.js

--- javascript/01.header-sidebar.js
+++ javascript/01.header-sidebar.js
@@ -33,7 +33,7 @@ function loadCSS(filename) {
   //set the attributes for the <link> element
   link.rel = 'stylesheet';
   link.type = 'text/css';
-  link.href = filename;
+  link.href = '/riverleasecondary/css/03.header-sidebar.css';
 
   //append the <link> element to the <head> of the document
   document.head.appendChild(link);
